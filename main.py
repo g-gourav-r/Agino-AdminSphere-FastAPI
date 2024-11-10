36ea4339b2aa667694507791a3d70cf8756084a3
g-gourav-r <gouravrchandra@gmail.com>
2024-11-11 00:34:35 +0530

    Add health check endpoint

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from routes import auth
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+
+@app.get("/", tags=["Health Check"])
+async def check_health_status():
+    return {"status": "success", "message": "The API endpoint is working fine"}
